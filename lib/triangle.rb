3aa906f2aa2b11566ef5320778501432ead18429
daragmas <daragmas@gmail.com>
2022-08-10 10:03:32 -0400

    commit tree 9d306e5a8ce2f78647c45b66ebee1c6f15f42297

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,30 @@
 class Triangle
-  # write code here
+  attr_reader :sides
+
+  def initialize side1, side2, side3
+    @sides = [side1, side2, side3].sort
+  end
+
+  def kind
+    if @sides.include?(0)
+      raise TriangleError
+    elsif @sides[0]+@sides[1] <= @sides[2]
+      raise TriangleError
+    else
+      triangle_type = if (sides.uniq).length == 1
+                        :equilateral
+                      elsif (sides.uniq).length == 2
+                        :isosceles
+                      else
+                        :scalene
+                      end
+    end
+  end
+
+  class TriangleError < StandardError
+    def message
+      "Invalid length(s) for sides of the triangle"
+    end
+  end
+
 end
